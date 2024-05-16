9860164c9de311d8ce40be958598f6ed8f662a1e
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-05-16 08:32:25 +0500

    Commit from Abdul-Muqaddam

--- SpecFlowProject1Selenium/obj/Release/net6.0/SpecFlowProject1Selenium.AssemblyInfo.cs
+++ SpecFlowProject1Selenium/obj/Release/net6.0/SpecFlowProject1Selenium.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowProject1Selenium")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4440c0604d615670dedeb983cf050fc0c96613")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowProject1Selenium")]
